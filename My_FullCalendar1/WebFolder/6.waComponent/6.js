6f15f8baf5cfd504962170dac6601f0885ad0ffa
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-11-25 14:11:37 +0100

    Initial commit

---
+++ My_FullCalendar1/WebFolder/6.waComponent/6.js
@@ -0,0 +1,97 @@
+﻿
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = '6';
+	// @endregion// @endlock
+
+	this.load = function (data) {// @lock
+	$(document).ready(function() {
+		
+		$('#component0').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2014-11-12',
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2014-11-01'
+				},
+				{
+					title: 'Long Event',
+					start: '2014-11-07',
+					end: '2014-11-10'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2014-11-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2014-11-16T16:00:00'
+				},
+				{
+					title: 'Conference',
+					start: '2014-11-11',
+					end: '2014-11-13'
+				},
+				{
+					title: 'Meeting',
+					start: '2014-11-12T10:30:00',
+					end: '2014-11-12T12:30:00'
+				},
+				{
+					title: 'Lunch',
+					start: '2014-11-12T12:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2014-11-12T14:30:00'
+				},
+				{
+					title: 'Happy Hour',
+					start: '2014-11-12T17:30:00'
+				},
+				{
+					title: 'Dinner',
+					start: '2014-11-12T20:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2014-11-13T07:00:00'
+				},
+				{
+					title: 'Click for Google',
+					url: 'http://google.com/',
+					start: '2014-11-28'
+				}
+			]
+		});
+		
+	});
+	// @region namespaceDeclaration// @startlock
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	// @region eventManager// @startlock
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
